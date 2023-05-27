97c322a98dc4f0a48319e01c22ddcc6704f3908a
Valeria <16-11233@usb.ve>
2023-05-27 14:15:05 -0400

    tweet

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Tweet from './components/tweets.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Tweet 
+        name='Shawn Wang'
+        username='shawn'
+        user_id=''
+        tweet='“On March 3, 1969 the United States Navy established an elite school for the top one percent of its pilots. Its purpose was to teach the lost art of aerial combat and to insure that the handful of men who graduated were the best fighter pilots in the world.”'
+        tweet_id='123456789'
+        day='12'
+        month='May'
+        year='2020'
+        retweets='10'
+        likes='20'
+        replies='30'
+      />
+            <Tweet 
+        name='Shawn Wang'
+        username='shawn'
+        user_id=''
+        tweet='sos'
+        tweet_id='123456789'
+        day='12'
+        month='May'
+        year='2020'
+        retweets='10'
+        likes='20'
+        replies='30'
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
