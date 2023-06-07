e03fa0ce531fa0a55a1c290b1a38abd7d9825242
ELKOMI2303 <16-10109@usb.ve>
2023-06-06 23:52:10 -0400

    obtener tweets y tweets de user_id

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import express from "express";
 import cors from "cors";
+import { router } from "./routes";
 import  dbConnect  from './config/mongo';
 const app = express();
 const PORT= process.env.PORT || 3001
 app.use(cors());
+app.use(express.json());
+app.use(router);
 
 
 
