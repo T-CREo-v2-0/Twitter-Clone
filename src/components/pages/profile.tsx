7426fc016da87142255a3b7bded6f9ddf1cbb2ff
Valeria <16-11233@usb.ve>
2023-06-08 18:25:02 -0400

    implemented a button for going back to TL

--- src/components/pages/profile.tsx
+++ src/components/pages/profile.tsx
@@ -1,11 +1,21 @@
-import { useParams } from 'react-router-dom';
-
+import { useParams, useNavigate } from 'react-router-dom';
 
 function Profile() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate('/')
+  }
+
   return (
+    <div className='text-black'>
+    <div className='absolute left-10 top-10'>
+        <button className="bg-transparent hover:bg-slate-600 text-slate-600 font-semibold hover:text-white py-2 px-4 border border-slate-600 hover:border-transparent rounded" type="button" onClick={navigateToHome}>Back to Timeline</button>
+    </div>
     <div className="container text-black">
        <h1>User Details Page - { id }</h1>
+    </div>
     </div>
   );
 }
