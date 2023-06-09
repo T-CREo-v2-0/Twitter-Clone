ab41336bc2d34eee28cb9a1d07cefe635cdcf435
fungikami <kafung09@gmail.com>
2023-06-09 00:32:54 -0400

    spinner

--- front-end/src/pages/Timeline.tsx
+++ front-end/src/pages/Timeline.tsx
@@ -1,45 +1,16 @@
 import { useEffect, useState } from "react";
-import Tweet from "../components/Tweet.js";
-import TwitterDB from "../api/client.js";
+import Tweet from "../components/Tweet";
+import Spinner from "../components/Spinner";
+import TwitterDB from "../api/client";
 import "../styles/timeline.css";
 
 const client = new TwitterDB();
 
-// const tweets = [
-//   {
-//     name: "Shawn Wang",
-//     username: "shawn",
-//     user_id: "",
-//     tweet:
-//       "“On March 3, 1969 the United States Navy established an elite school for the top one percent of its pilots. Its purpose was to teach the lost art of aerial combat and to insure that the handful of men who graduated were the best fighter pilots in the world.”",
-//     tweet_id: "123456789",
-//     day: "12",
-//     month: "May",
-//     year: "2020",
-//     retweets: "10",
-//     likes: "20",
-//     replies: "30",
-//   },
-//   {
-//     name: "Shawn Wang",
-//     username: "shawn",
-//     user_id: "",
-//     tweet: "sos",
-//     tweet_id: "9999999",
-//     day: "12",
-//     month: "May",
-//     year: "2020",
-//     retweets: "10",
-//     likes: "20",
-//     replies: "30",
-//   },
-// ];
-
 function Timeline() {
   const [isLoading, setIsLoading] = useState(true);
-  
+
   // Get tweets from database
-  const [tweets, setTweets] : any = useState([]);
+  const [tweets, setTweets]: any = useState([]);
   useEffect(() => {
     client.getTweets(30).then((res) => {
       console.log(res);
@@ -48,13 +19,11 @@ function Timeline() {
     });
   }, []);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  if (isLoading) return <Spinner />;
 
   return (
     <div className="Timeline">
-      {tweets.map((tweet : any) => (
+      {tweets.map((tweet: any) => (
         <Tweet
           avatar={tweet.user.profile_image_url_https}
           name={tweet.user.name}
@@ -64,7 +33,7 @@ function Timeline() {
           time={tweet.created_at}
           retweets={tweet.retweet_count}
           likes={tweet.favorite_count}
-          replies={'100'}
+          replies={"100"}
         />
       ))}
     </div>
