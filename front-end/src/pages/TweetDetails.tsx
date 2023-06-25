f49259b0beba42114188e2adfed9702290824e8e
fungikami <kafung09@gmail.com>
2023-06-25 11:55:50 -0400

    fix tweet detail

--- front-end/src/pages/TweetDetails.tsx
+++ front-end/src/pages/TweetDetails.tsx
@@ -1,5 +1,6 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import Tweet from "../components/Tweet";
+import "../styles/timeline.css";
 
 function TweetDetails() {
   const navigate = useNavigate();
@@ -11,7 +12,7 @@ function TweetDetails() {
   const tweet = location.state;
 
   return (
-    <>
+    <div className="text-black">
       <div className="absolute left-10 top-10">
         <button
           className="bg-transparent hover:bg-slate-600 text-slate-600 font-semibold hover:text-white py-2 px-4 border border-slate-600 hover:border-transparent rounded"
@@ -21,10 +22,12 @@ function TweetDetails() {
           Back to Timeline
         </button>
       </div>
-      <div className="w-full bg-white mb-4 rounded-xl overflow-hidden max-w-xl min-w-xl text-black">
-        <Tweet tweet={ tweet } />
+      <div className="container text-black">
+        <div>
+          <Tweet tweet={tweet} />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
